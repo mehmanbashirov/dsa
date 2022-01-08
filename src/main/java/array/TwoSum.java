5f71608809546aa3095c98c73edc22fae908786e
bashirovma <50512710511!M>
2022-01-09 02:33:29 +0400

    Added TwoSum algorithm

--- src/main/java/array/TwoSum.java
+++ src/main/java/array/TwoSum.java
@@ -1,4 +1,28 @@
 package array;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null) {
+            return null;
+        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int expected = target - nums[i];
+            if (map.containsKey(expected)) {
+                return new int[]{i, map.get(expected)};
+            }
+            map.put(nums[i], i);
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        TwoSum twoSum = new TwoSum();
+        System.out.println(Arrays.toString(twoSum.twoSum(new int[]{1, 2, 3, 4, 5, 0}, 9)));
+    }
+
 }
